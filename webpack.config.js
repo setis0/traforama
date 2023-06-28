c5c9ec611673f7f393b4ace6e1ae38a90a6e56c1
Евгений Евтихов <124mgp@gmail.com>
2023-06-28 16:05:40 +0700

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,50 @@
-var webpack = require('webpack')
-    , path = require('path')
-    // , TerserPlugin = require("terser-webpack-plugin")
-    , env = process.env.WEBPACK_ENV
-    ;
+var webpack = require('webpack'),
+  path = require('path'),
+  // , TerserPlugin = require("terser-webpack-plugin")
+  env = process.env.WEBPACK_ENV;
 console.log(env);
-var libraryName = 'ats-lib-clickadu'
-    , outputFile = ''
-    , plugins = []
-    ;
-
+var libraryName = 'ats-lib-clickadu',
+  outputFile = '',
+  plugins = [];
 // configure output for proper build type
 if (env === 'build') {
-    // plugins.push(new TerserPlugin());
-    outputFile = 'index' + '.min.js';
+  // plugins.push(new TerserPlugin());
+  outputFile = 'index' + '.min.js';
 } else {
-    outputFile = 'index' + '.js';
+  outputFile = 'index' + '.js';
 }
 
 module.exports = {
-    entry: path.join(__dirname, 'src', 'index.ts'),
-    output: {
-        path: path.join(__dirname, 'lib'),
-        filename: outputFile,
-        library: {
-            name: libraryName,
-            type: 'umd'
-        },
-        // library: libraryName,
-        // libraryTarget: 'umd',
-        umdNamedDefine: true,
-        globalObject: `(typeof self !== 'undefined' ? self : this)`
+  entry: path.join(__dirname, 'src', 'index.ts'),
+  output: {
+    path: path.join(__dirname, 'lib'),
+    filename: outputFile,
+    library: {
+      name: libraryName,
+      type: 'umd'
     },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                loader: 'ts-loader',
-                // exclude: /(node_modules)/
-            },
-            {
-                test: /(\.jsx|\.js)$/,
-                loader: 'babel',
-                exclude: /(node_modules)/
-            }
-        ]
-    },
-    externals: ['axios'],
-    resolve: {
-        modules: [path.resolve('./src')],
-        extensions: ['.js', '.ts']
-    }
-}
+    // library: libraryName,
+    // libraryTarget: 'umd',
+    umdNamedDefine: true,
+    globalObject: `(typeof self !== 'undefined' ? self : this)`
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader'
+        // exclude: /(node_modules)/
+      },
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'babel',
+        exclude: /(node_modules)/
+      }
+    ]
+  },
+  externals: ['axios', 'ats-lib-ntwk-common'],
+  resolve: {
+    modules: [path.resolve('./src')],
+    extensions: ['.js', '.ts']
+  }
+};
